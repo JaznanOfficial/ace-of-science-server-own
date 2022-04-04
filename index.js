be2ba5e12fec84ebe34530b10a5cedcb539215f8
JaznanOfficial <jaznanofficial@gmail.com>
2022-04-04 15:56:38 +0600

    delete done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
+const ObjectId = require('mongodb').ObjectId;
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
@@ -46,7 +47,8 @@ async function run() {
           const blogs = req.body;
           console.log(blogs);
             const blogResult = await blogCollection.insertOne(blogs)
-            // console.log(blogResult);
+          console.log(blogResult);
+          res.send(blogResult)
             
         })
       // blog data post--------------------------------->
@@ -67,20 +69,33 @@ async function run() {
         console.log(profile);
           const profileResult = await profileCollection.insertOne(profile)
           // console.log(profileResult);
-          
+          res.json(profileResult)
       })
       
       // profile update post---------------------------->
 
-      // blog data get---------------------------------->
+      // profile data get---------------------------------->
       app.get('/profile', async (req, res) => {
         const cursor = profileCollection.find({});
         const getProfile = await cursor.toArray();
         res.send(getProfile);
       })
-        // blog data get-------------------------------->
+        // profile data get-------------------------------->
+
+      // blog data delete------------------------------>
+
+      app.delete('/blogs/:id', async (req, res) => {
+        // console.log(req.params.id);
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await blogCollection.deleteOne(query)
+        console.log(result);
+        res.json(result)
+        })
 
 
+
+      // blog data delete------------------------------>
 
 
 
