9239b2699cc7b9d8d8fb4075e0ac1675314b9d76
JaznanOfficial <jaznanofficial@gmail.com>
2022-04-14 20:00:09 +0600

    jwt done

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
-const ObjectId = require('mongodb').ObjectId;
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const ObjectId = require("mongodb").ObjectId;
 // const userEmail = require('mongodb').userEmail;
 
 const app = express();
 const port = process.env.PORT || 5000;
-const cors = require('cors');
+const cors = require("cors");
 
+// firebase admin initialization
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./ace-of-science-firebase-adminsdk-nn20j-432cb86bc9.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
 
 app.use(cors());
 app.use(express.json());
@@ -18,182 +27,160 @@ app.use(express.json());
 // connecting database ---------------------->
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rrls8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
 // connecting database--------------------------->
 
+// verify token----------------------->
+
+async function verifyToken(req, res, next) {
+    if (req.headers?.authorization?.startsWith("Bearer ")) {
+        const idToken = req.headers.authorization.split("Bearer ")[1];
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(idToken);
+            // console.log('email',decodedUser.email);
+            req.decodedEmail = decodedUser.email;
+        } catch {}
+    }
+
+    next();
+}
+// verify token------------------------->
 
 async function run() {
     try {
-      await client.connect();
-      const database = client.db("Ace-of-Science-own");
-      const reviewCollection = database.collection("Review");
-      const blogCollection = database.collection("Blogs");
-      const profileCollection = database.collection('Profile');
-      // create a document to insert
+        await client.connect();
+        const database = client.db("Ace-of-Science-own");
+        const reviewCollection = database.collection("Review");
+        const blogCollection = database.collection("Blogs");
+        const profileCollection = database.collection("Profile");
+        // create a document to insert
 
-      // Review Data post --------------------------->
-        app.post('/review', async(req, res) => {
-          const review = req.body;
+        // Review Data post --------------------------->
+        app.post("/review", async (req, res) => {
+            const review = req.body;
 
-            const reviewResult = await reviewCollection.insertOne(review)
-          res.send(reviewResult)
-        })
-      // review data post ----------------------------->
+            const reviewResult = await reviewCollection.insertOne(review);
+            res.send(reviewResult);
+        });
+        // review data post ----------------------------->
 
+        // Review Data get --------------------------->
+        app.get("/review", async (req, res) => {
+            const cursor = reviewCollection.find({});
+            const getReview = await cursor.toArray();
+            res.send(getReview);
+        });
+        // review data get ----------------------------->
+        // review data delete------------------------------>
 
-      // Review Data get --------------------------->
-        app.get('/review', async(req, res) => {
-          const cursor = reviewCollection.find({});
-        const getReview = await cursor.toArray();
-        res.send(getReview); 
-        })
-      // review data get ----------------------------->
-       // review data delete------------------------------>
+        app.delete("/review/:id", async (req, res) => {
+            // console.log(req.params.id);
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            // console.log(result);
+            res.json(result);
+        });
 
-       app.delete('/review/:id', async (req, res) => {
-        // console.log(req.params.id);
-        const id = req.params.id;
-        const query = { _id: ObjectId(id) };
-        const result = await reviewCollection.deleteOne(query)
-        // console.log(result);
-        res.json(result)
-        })
+        // review data delete------------------------------>
 
+        // blog data post------------------------------->
+        app.post("/blogs", async (req, res) => {
+            const blogs = req.body;
+            // console.log(blogs);
+            const blogResult = await blogCollection.insertOne(blogs);
+            // console.log(blogResult);
+            res.send(blogResult);
+        });
+        // blog data post--------------------------------->
 
-
-      // review data delete------------------------------>
-      
-
-
-      // blog data post------------------------------->
-        app.post('/blogs', async(req, res) => {
-          const blogs = req.body;
-          console.log(blogs);
-            const blogResult = await blogCollection.insertOne(blogs)
-          console.log(blogResult);
-          res.send(blogResult)
-            
-        })
-      // blog data post--------------------------------->
-
-      // single blog data get---------------------------------->
-      app.get('/single-blog/:id', async (req, res) => {
-        console.log(req.params.id);
-        const id = req.params.id;
-        const query = { _id: ObjectId(id) }
-        console.log(query);
-        const cursor = await blogCollection.findOne(query);
-        console.log(cursor);
-        res.send(cursor);
-      })
+        // single blog data get---------------------------------->
+        app.get("/single-blog/:id", async (req, res) => {
+            // console.log(req.params.id);
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            // console.log(query);
+            const cursor = await blogCollection.findOne(query);
+            // console.log(cursor);
+            res.send(cursor);
+        });
         // single blog data get-------------------------------->
-      
-      
-      // blog data get---------------------------------->
-      app.get('/blogs', async (req, res) => {
-        const cursor = blogCollection.find({});
-        const getBlog = await cursor.toArray();
-        res.send(getBlog);
-      })
+
+        // blog data get---------------------------------->
+        app.get("/blogs", async (req, res) => {
+            const cursor = blogCollection.find({});
+            const getBlog = await cursor.toArray();
+            res.send(getBlog);
+        });
         // blog data get-------------------------------->
-      
-       // blog data delete------------------------------>
 
-       app.delete('/blogs/:id', async (req, res) => {
-        // console.log(req.params.id);
-        const id = req.params.id;
-        const query = { _id: ObjectId(id) };
-        const result = await blogCollection.deleteOne(query)
-        console.log(result);
-        res.json(result)
-        })
+        // blog data delete------------------------------>
 
+        app.delete("/blogs/:id", async (req, res) => {
+            // console.log(req.params.id);
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await blogCollection.deleteOne(query);
+            // console.log(result);
+            res.json(result);
+        });
 
+        // blog data delete------------------------------>
 
-      // blog data delete------------------------------>
-      
-      // profile update post---------------------------->
+        // profile update post---------------------------->
 
-    
-      app.put('/profile/:userEmail', async(req, res) => {
-        const profile = req.body;
-        const userEmail = req.params.userEmail;
-        // console.log(profile, userEmail);
-        const query = { userEmail: userEmail };
-        const options = { upsert: true };
-        const updateUser = {
-          $set: {
-            address: profile.address,
-            school: profile.school,
-            phone: profile.phone,
-            profession: profile.profession
+        app.put("/profile/:userEmail", async (req, res) => {
+            const profile = req.body;
+            const userEmail = req.params.userEmail;
+            // console.log(profile, userEmail);
+            const query = { userEmail: userEmail };
+            const options = { upsert: true };
+            const updateUser = {
+                $set: {
+                    address: profile.address,
+                    school: profile.school,
+                    phone: profile.phone,
+                    profession: profile.profession,
+                },
+            };
+            const profileResult = await profileCollection.updateOne(query, updateUser, options);
+            console.log(profileResult);
+            // res.json(profileResult)
+        });
+
+        // profile update post---------------------------->
+
+        // profile data get---------------------------------->
+        app.get("/profile", verifyToken, async (req, res) => {
+            // console.log(req.query);
+
+            const userEmail = req.query.email;
+
+            if (req.decodedEmail === userEmail) {
+                const query = { userEmail: userEmail };
+                const cursor = await profileCollection.findOne(query);
+                res.send(cursor);
           }
-        }
-          const profileResult = await profileCollection.updateOne(query,updateUser,options)
-          console.log(profileResult);
-          // res.json(profileResult)
-      })
-      
-      // profile update post---------------------------->
-
-      // profile data get---------------------------------->
-      app.get('/profile', async (req, res) => {
-        console.log(req.query);
-        console.log(req.headers);
-        let query = {}
-        const userEmail = req.query.email;
-        if (userEmail) {
-          query = { userEmail: userEmail }; 
-        }
-
-        
-        
-        const cursor = await profileCollection.findOne(query);
-        // console.log(cursor);
-        
-        res.send(cursor);
-      })
+            else {
+              res.status(401).send('You Have No Permission to access this data. Cause you are not authorized')
+          }
+            // console.log(cursor);
+        });
         // profile data get-------------------------------->
-
-     
-
-
-
-
-        
-        
-      
     } finally {
-    //   await client.close();
+        //   await client.close();
     }
-  }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.get('/', (req, res) => {
-    res.send('Hello world');
-})
-
+app.get("/", (req, res) => {
+    res.send("Hello world");
+});
 
 app.listen(port, () => {
-    console.log('server is running on port', port);
-})
+    console.log("server is running on port", port);
+});
